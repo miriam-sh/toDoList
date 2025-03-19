c8293fa9d03167852e5a31e4b3e812883c509c65
miriam-sh <m0534106023@Gmail.com>
2025-03-19 15:16:23 +0200

    Update Register.jsx

--- ToDoListReact/src/Register.jsx
+++ ToDoListReact/src/Register.jsx
@@ -7,24 +7,26 @@ export const Register = () => {
 
     const nav = useNavigate()
 
+    const register = async (e) => {
+        e.preventDefault();
+
+        let user = {
+            username: e.target[0].value,
+            password: e.target[2].value
+        }
+
+        await service.register(user)
+
+        nav("../manageToDoList")
+    }
+
     return <>
-        <form onSubmit={async (e) => {
-
-            e.preventDefault();
-
-            let user = {
-                username: e.target[0].value,
-                password: e.target[2].value
-            }
-
-            await service.register(user)
-
-            nav("../manageToDoList")
-
-        }}>
-            <TextField label="user name"></TextField>
-            <TextField type="password" label="password" variant="outlined"></TextField>
-            <Button startIcon={<LoginIcon></LoginIcon>} type="submit" variant="outlined">הרשמה</Button>
-        </form>
+        <div className="register">
+            <form className="form" onSubmit={async (e) => { register(e) }}>
+                <TextField required className="inputTextField" label="user name"></TextField>
+                <TextField required className="inputTextField" type="password" label="password" variant="outlined"></TextField>
+                <Button id="submitInput" className="inputTextField" startIcon={<LoginIcon></LoginIcon>} type="submit" variant="outlined">הרשמה</Button>
+            </form>
+        </div>
     </>
 }
