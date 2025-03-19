4bc38728df3b5e760df18cacab94f37688c9870c
miriam-sh <m0534106023@Gmail.com>
2025-03-19 15:16:08 +0200

    Update ManageToDoList.jsx

--- ToDoListReact/src/ManageToDoList.jsx
+++ ToDoListReact/src/ManageToDoList.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import service from './service.js';
+import { IconButton } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home'
+import { useNavigate } from 'react-router';
 
 export const ManageToDoList = () => {
     const [newTodo, setNewTodo] = useState("");
@@ -27,33 +30,42 @@ export const ManageToDoList = () => {
         await getTodos();//refresh tasks list
     }
 
+    const nav = useNavigate()
+
     useEffect(() => {
         getTodos();
     }, []);
 
     return (
-        <section className="todoapp">
-            <header className="header">
-                <h1>todos</h1>
-                <form onSubmit={createTodo}>
-                    <input className="new-todo" placeholder="Well, let's take on the day" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
-                </form>
-            </header>
-            <section className="main" style={{ display: "block" }}>
-                <ul className="todo-list">
-                    {todos.map(todo => {
-                        return (
-                            <li className={todo.isComplete ? "completed" : ""} key={todo.id}>
-                                <div className="view">
-                                    <input className="toggle" type="checkbox" defaultChecked={todo.isComplete} onChange={(e) => updateCompleted(todo, e.target.checked)} />
-                                    <label>{todo.name}</label>
-                                    <button className="destroy" onClick={() => deleteTodo(todo.id)}></button>
-                                </div>
-                            </li>
-                        );
-                    })}
-                </ul>
-            </section>
-        </section >
+        <>
+            <IconButton
+                style={{ marginTop: "8%" }}
+                onClick={() => { nav("/login") }}
+                className="roundButton"><HomeIcon></HomeIcon>
+            </IconButton>
+            <section className="todoapp">
+                <header className="header">
+                    <h1>todos</h1>
+                    <form onSubmit={createTodo}>
+                        <input className="new-todo" placeholder="Well, let's take on the day" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
+                    </form>
+                </header>
+                <section className="main" style={{ display: "block" }}>
+                    <ul className="todo-list">
+                        {todos.map(todo => {
+                            return (
+                                <li className={todo.isComplete ? "completed" : ""} key={todo.id}>
+                                    <div className="view">
+                                        <input className="toggle" type="checkbox" defaultChecked={todo.isComplete} onChange={(e) => updateCompleted(todo, e.target.checked)} />
+                                        <label>{todo.name}</label>
+                                        <button className="destroy" onClick={() => deleteTodo(todo.id)}></button>
+                                    </div>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </section>
+            </section >
+        </>
     );
 }
