5cf2788ff8a5d0f04c32d2a275a061b8d36bfa1f
miriam-sh <m0534106023@Gmail.com>
2025-03-19 15:15:35 +0200

    Update Login.jsx

--- ToDoListReact/src/Login.jsx
+++ ToDoListReact/src/Login.jsx
@@ -8,24 +8,32 @@ export const Login = () => {
 
     const nav = useNavigate()
 
+    useEffect(() => {        
+        if (window.location.href[window.location.href.length - 1] != 'n')
+            nav("./login")
+    })
+
+    const login = async (e) => {
+        e.preventDefault();
+
+        let user = {
+            username: e.target[0].value,
+            password: e.target[2].value
+        }
+
+        await service.login(user)
+
+        nav("../manageToDoList")
+    }
+
     return <>
-        <form onSubmit={async (e) => {
-
-            e.preventDefault();
-
-            let user = {
-                username: e.target[0].value,
-                password: e.target[2].value
-            }
-
-            await service.login(user)
-
-            nav("../manageToDoList")
-        }}>
-            <TextField label="user name"></TextField>
-            <TextField type="password" label="password" variant="outlined"></TextField>
-            <Button startIcon={<LoginIcon></LoginIcon>} type="submit" variant="outlined">כניסה</Button>
-        </form>
+        <div className="login">
+            <form className="form" onSubmit={async (e) => { login(e) }}>
+                <TextField required className="inputTextField" label="user name"></TextField>
+                <TextField required className="inputTextField" type="password" label="password" variant="outlined"></TextField>
+                <Button id="submitInput" className="inputTextField" startIcon={<LoginIcon></LoginIcon>} type="submit" variant="outlined">כניסה</Button>
+            </form>
+        </div>
     </>
 
 }
